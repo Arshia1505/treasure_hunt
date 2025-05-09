4288100e1afb96301984ea89ee99e0f707f81ac4
Arshia Sharma <arshia15854@gmail.com>
2025-05-09 19:08:01 +0530

    Added .env.local file to .gitignore

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -40,12 +40,18 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCaESVnbrlIbJYQ8-jddPy9BIuJCxY3HD8",
-  authDomain: "treasurehunt-c1ef0.firebaseapp.com",
-  projectId: "treasurehunt-c1ef0",
-  storageBucket: "treasurehunt-c1ef0.appspot.com", // ✅ FIXED
-  messagingSenderId: "976003041251",
-  appId: "1:976003041251:web:674a042d62cef58f5ac2d6",
+  // apiKey: "AIzaSyCaESVnbrlIbJYQ8-jddPy9BIuJCxY3HD8",
+  // authDomain: "treasurehunt-c1ef0.firebaseapp.com",
+  // projectId: "treasurehunt-c1ef0",
+  // storageBucket: "treasurehunt-c1ef0.appspot.com", // ✅ FIXED
+  // messagingSenderId: "976003041251",
+  // appId: "1:976003041251:web:674a042d62cef58f5ac2d6",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,  
   measurementId: "G-0JVZ9BFYD9"
 };
 
